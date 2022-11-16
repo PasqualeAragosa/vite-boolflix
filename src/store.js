92b67e337b9dd24afba4f0e69ac5cae23ffe55e0
Pasquale Aragosa <aragosap@gmail.com>
2022-11-16 14:57:09 +0100

    inizial commit

---
+++ src/store.js
@@ -0,0 +1,25 @@
+import { reactive } from 'vue';
+import axios from 'axios';
+
+export const store = reactive({
+    API_URL: 'https://api.themoviedb.org/3/search/movie?api_key=8902f37ceb167968be0ac7d84334f2bf&language=en-US&query=',
+    movies: '',
+    original_title: '',
+    language: '',
+    rate: 0,
+
+    callApi(nameMovie) {
+        const url = `${this.API_URL}${nameMovie}`;
+        console.log(url);
+
+        axios
+            .get(url)
+            .then(response => {
+                console.log(response.data);
+                this.movies = response.data.results;
+            })
+            .catch(error => {
+                console.log(error.message);
+            })
+    }
+})
